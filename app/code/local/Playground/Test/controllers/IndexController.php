643c997635371baf45a0310edcadabb25fb38f87
vaidasif <vaidasif@gmail.com>
2014-08-06 17:15:07 +0300

    Create a new template, which will contain the static text from the previous sentence and remove it from the controller. Please use the translator method, so your text can be translated to any languages.

--- app/code/local/Playground/Test/controllers/IndexController.php
+++ app/code/local/Playground/Test/controllers/IndexController.php
@@ -4,6 +4,7 @@ class Playground_Test_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-        echo 'Today is a happy day';
+        $this->loadLayout();
+        $this->renderLayout();
     }
 }
